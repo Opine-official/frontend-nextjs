b145ae3d42f49b460036ac690ee4237544ce151f
Aravind Sanjeev <saravind436@gmail.com>
2024-01-08 19:49:55 +0530

    Add user verification on feed page

--- src/app/(pages)/feed/page.tsx
+++ src/app/(pages)/feed/page.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+import axiosInstance from "@/shared/helpers/axiosInstance";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const FeedPage: React.FC = () => {
+  const router = useRouter();
+
+  async function verifyUser() {
+    try {
+      const response = await axiosInstance.get("/feed/verifyUser");
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+      router.push("/login");
+    }
+  }
+
+  useEffect(() => {
+    try {
+      verifyUser();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center h-screen">
