40fc0bdb32fbda83380a1102f6ccb2bfe80f4a0a
Aravind Sanjeev <saravind436@gmail.com>
2024-01-13 10:29:42 +0530

    Refactor page component and add article fetching functionality

--- src/app/(pages)/editor/page.tsx
+++ src/app/(pages)/editor/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState } from "react";
-import Navbar from "@/app/components/Navbar";
+import { useEffect, useState } from "react";
 import EditorContainer from "./components/EditorContainer";
 import ActionsTray from "./components/ActionsTray";
 import { ProgressBar } from "react-loader-spinner";
+import axiosInstance from "@/shared/helpers/axiosInstance";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const INITIAL_META_DATA = {
   title: "",
@@ -24,11 +25,42 @@ const INITIAL_DATA = {
 };
 
 function Page() {
+  const slug = useSearchParams().get("slug") ?? null;
+
+  const router = useRouter();
+
   const [metaData, setMetaData] = useState(INITIAL_META_DATA);
   const [data, setData] = useState(INITIAL_DATA);
-  const [loading, setLoading] = useState(false);
+  const [saving, setSaving] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  if (loading) {
+  async function fetchArticle() {
+    try {
+      const res = await axiosInstance.get(`/post/?slug=${slug}`);
+      console.log(res.data);
+      const data = res.data;
+      setMetaData({
+        title: data.title,
+        description: data.description,
+        tags: data.tags,
+      });
+      setData(data.content);
+    } catch (error) {
+      console.error(error);
+      router.push("/posts");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (slug) {
+      fetchArticle();
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (saving || loading) {
     return (
       <div className="h-screen flex flex-col justify-center items-center bg-gray-200 transition-opacity duration-500 ease-in-out opacity-100">
         <ProgressBar
@@ -42,7 +74,9 @@ function Page() {
           wrapperClass=""
         />
         <p className="mt-0 text-2xl text-gray-700">
-          Baking your article into the database
+          {saving
+            ? "Baking your article into the database"
+            : "Good things take time"}
           <span className="text-4xl animate-ping">.</span>
           <span className="text-4xl animate-ping delay-150">.</span>
           <span className="text-4xl animate-ping delay-300">.</span>
@@ -57,8 +91,14 @@ function Page() {
         data={data}
         setData={setData}
         setMetaData={setMetaData}
+        metaData={metaData}
       />
-      <ActionsTray metaData={metaData} data={data} setLoading={setLoading} />
+      <ActionsTray
+        metaData={metaData}
+        data={data}
+        setLoading={setSaving}
+        slug={slug}
+      />
     </div>
   );
 }
