1fc9a4de923a96261a7e1f09418734e74960ef1b
Aravind Sanjeev <saravind436@gmail.com>
2024-01-13 17:18:13 +0530

    Add new components and dependencies

---
+++ src/app/(pages)/editor/components/PostSettings.tsx
@@ -0,0 +1,33 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { MdOutlineSettings } from "react-icons/md";
+import PostSettingsForm from "./PostSettingsForm";
+
+type Props = {};
+
+const PostSettings = (props: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <MdOutlineSettings className="cursor-pointer" size={28} />
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-5">Post settings</DialogTitle>
+          <DialogDescription>
+            <PostSettingsForm />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PostSettings;
---
+++ src/app/(pages)/editor/components/PostSettingsForm.tsx
@@ -0,0 +1,126 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Tag, TagInput } from "@/components/tag/tag-input";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { toast } from "@/components/ui/use-toast";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+
+const FormSchema = z.object({
+  tags: z.array(
+    z
+      .object({
+        id: z.string(),
+        text: z.string(),
+      })
+      .optional()
+  ),
+  slug: z.string().optional(),
+  isThreadsEnabled: z.boolean().default(true).optional(),
+});
+
+export default function PostSettingsForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      tags: [],
+      slug: "",
+      isThreadsEnabled: true,
+    },
+  });
+
+  const [tags, setTags] = React.useState<Tag[]>([]);
+
+  const { setValue } = form;
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    alert(JSON.stringify(data, null, 2));
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 flex flex-col items-start "
+      >
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-start">
+              <FormLabel className="text-left">Tags</FormLabel>
+              <FormControl>
+                <TagInput
+                  {...field}
+                  maxTags={5}
+                  placeholder="Start typing to add a tag"
+                  tags={tags}
+                  className="sm:min-w-[450px]"
+                  setTags={(newTags) => {
+                    setTags(newTags);
+                    setValue("tags", newTags as [Tag, ...Tag[]]);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                Tags allow you to publish to a specific channel. You can add up
+                to 5 tags.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="slug"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-start">
+              <FormLabel className="text-left">Slug</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="whats-the-headline-for-your-story"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                You can assign a custom slug. If you leave it blank, we&apos;ll
+                auto generate one for you.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="isThreadsEnabled"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-start">
+              <FormLabel className="text-left">Publish on threads</FormLabel>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Save</Button>
+      </form>
+    </Form>
+  );
+}
